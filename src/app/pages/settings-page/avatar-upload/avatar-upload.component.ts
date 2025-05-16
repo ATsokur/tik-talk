e879d4708608ae04332ef8791797a72e6c396b6d
ATsokur <flamer721@gmail.com>
2025-05-16 16:17:45 +0300

    Add upload existing avatar in settings page

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,17 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+
+import { tap } from 'rxjs';
 
 import { DndDirective } from '../../../common-ui/directives/dnd.directive';
 import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
+import { ProfileService } from '../../../data/services/profile.service';
 
 @Component({
   selector: 'app-avatar-upload',
-  imports: [SvgIconComponent, DndDirective],
+  imports: [SvgIconComponent, DndDirective, AsyncPipe],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss',
 })
 export class AvatarUploadComponent {
-  public preview = signal<string>('/assets/img/avatar-placeholder.png');
-  avatar: File | null = null;
+  private readonly profileService = inject(ProfileService);
+  public preview = signal<string>('');
+
+  public me$ = toObservable(this.profileService.me).pipe(
+    tap((profile) => {
+      profile?.avatarUrl
+        ? this.preview.set(
+            `https://icherniakov.ru/yt-course/${profile.avatarUrl}`
+          )
+        : this.preview.set('/assets/img/avatar-placeholder.png');
+    })
+  );
+
+  public avatar: File | null = null;
 
   fileBrowserHandler(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
