841c32b3ef09a61f3bdeb4b8abbc80860e7b7db3
ATsokur <flamer721@gmail.com>
2025-07-13 16:01:43 +0300

    Add resize for chat-list

--- libs/chats/src/lib/feature-chats-workspace/chats-list/chats-list.component.ts
+++ libs/chats/src/lib/feature-chats-workspace/chats-list/chats-list.component.ts
@@ -1,11 +1,29 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { ChatsBtnComponent } from '../chats-btn/chats-btn.component';
 import { AsyncPipe } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, map, startWith, switchMap } from 'rxjs';
-import { ChatsService } from '@tt/data-access';
+import {
+  AfterViewInit,
+  Component,
+  DestroyRef,
+  ElementRef,
+  inject,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import {
+  auditTime,
+  debounceTime,
+  fromEvent,
+  map,
+  startWith,
+  switchMap
+} from 'rxjs';
+
+import { ChatsService } from '@tt/data-access';
+
+import { ChatsBtnComponent } from '../chats-btn/chats-btn.component';
 
 @Component({
   selector: 'app-chats-list',
@@ -19,9 +37,12 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './chats-list.component.html',
   styleUrl: './chats-list.component.scss'
 })
-export class ChatsListComponent {
+export class ChatsListComponent implements AfterViewInit {
   private readonly chatsService = inject(ChatsService);
+  #r2 = inject(Renderer2);
   #destroy$ = inject(DestroyRef);
+  @ViewChild('chatsWrap') chatsWrapElement!: ElementRef;
+  hostElement = inject(ElementRef);
 
   public filterChatsControl = new FormControl<string>('');
 
@@ -41,4 +62,21 @@ export class ChatsListComponent {
       );
     })
   );
+
+  resizeFeed(): void {
+    const { top } = this.chatsWrapElement.nativeElement.getBoundingClientRect();
+    const height = window.innerHeight - top - 13 - 12;
+    this.#r2.setStyle(
+      this.chatsWrapElement.nativeElement,
+      'height',
+      `${height}px`
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.resizeFeed();
+    fromEvent(window, 'resize')
+      .pipe(auditTime(300), takeUntilDestroyed(this.#destroy$))
+      .subscribe(() => this.resizeFeed());
+  }
 }
